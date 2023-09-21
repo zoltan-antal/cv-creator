1cc4807374a32da11cbe67961b072e5f86578b66
zolantal <zoltan_antal@outlook.com>
2023-09-21 17:53:54 +0100

    Display full name

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,7 +1,17 @@
 import '../styles/Page.css';
+import { useCvData } from '../utils/CvDataContext';
 
 function Page() {
-  return <div className="page"></div>;
+  const cvData = useCvData();
+  const savedCvData = cvData.savedCvData;
+
+  return (
+    <div className="page">
+      <div className="personal-details">
+        {savedCvData.personalDetails.fullName}
+      </div>
+    </div>
+  );
 }
 
 export default Page;
