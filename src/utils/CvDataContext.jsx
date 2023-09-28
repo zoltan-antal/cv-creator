9cb417b658a044e9e2258903675d6984eecff124
zolantal <zoltan_antal@outlook.com>
2023-09-28 12:34:43 +0100

    Fix dates not parsing as dates from localstorage

--- src/utils/CvDataContext.jsx
+++ src/utils/CvDataContext.jsx
@@ -7,7 +7,7 @@ import blankCv from '../dataStructures/blankCv';
 const CvDataContext = createContext(null);
 const CvDataDispatchContext = createContext(null);
 
-const localCvData = JSON.parse(localStorage.getItem('cvData'));
+const localCvData = parseDates(JSON.parse(localStorage.getItem('cvData')));
 const initialCvData = {
   savedCvData: localCvData || blankCv,
   tempCvData: localCvData || blankCv,
@@ -127,4 +127,20 @@ function cvDataReducer(cvData, action) {
     default:
       throw Error('Unknown action: ' + action.type);
   }
+}
+
+function parseDates(obj) {
+  for (var key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      // If the property is an object, recursively parse it
+      obj[key] = parseDates(obj[key]);
+    } else if (
+      typeof obj[key] === 'string' &&
+      obj[key].match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)
+    ) {
+      // If the property is a string in ISO date format, convert it to a Date object
+      obj[key] = new Date(obj[key]);
+    }
+  }
+  return obj;
 }
