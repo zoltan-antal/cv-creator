6a0a07b32d337c9a0d4d14b4038ff01ab35c42bb
zolantal <zoltan_antal@outlook.com>
2023-09-28 11:20:26 +0100

    Change page to grid layout

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -11,71 +11,78 @@ function Page() {
 
   return (
     <div className="page">
-      <div className="personal-details">
-        <div className="personal-info">
-          {Object.entries(cvData.personalDetails).map(([key, value]) => {
-            if (key.includes('fullName') && value) {
-              return (
-                <h1 className="name" key={key}>
-                  {value}
-                </h1>
-              );
-            } else if (key.includes('professionalTitle') && value) {
-              return (
-                <h2 className="title" key={key}>
-                  {value}
-                </h2>
-              );
-            } else if (key.includes('professionalSummary') && value) {
-              return (
-                <pre className="summary" key={key}>
-                  {value}
-                </pre>
-              );
-            }
-          })}
-        </div>
-        <div className="personal-data">
-          {Object.entries(cvData.personalDetails).map(([key, value]) => {
-            if (key.includes('email') && value) {
-              return (
-                <div className="entry email" key={key}>
-                  <img src={emailIcon} alt="" />
-                  <p>{value}</p>
-                </div>
-              );
-            } else if (key.includes('phone') && value) {
-              return (
-                <div className="entry phone" key={key}>
-                  <img src={phoneIcon} alt="" />
-                  <p>{value}</p>
-                </div>
-              );
-            } else if (key.includes('address') && value) {
-              return (
-                <div className="entry address" key={key}>
-                  <img src={addressIcon} alt="" />
-                  <p>{value}</p>
-                </div>
-              );
-            } else if (key.includes('links')) {
-              return (
-                <>
-                  {value.map((value, index) => {
-                    if (value) {
-                      return (
-                        <div className="entry link" key={index}>
-                          <img src={linkIcon} alt="" />
-                          <p>{value}</p>
-                        </div>
-                      );
-                    }
-                  })}
-                </>
-              );
-            }
-          })}
-        </div>
+      <div className="personal-info">
+        {Object.entries(cvData.personalDetails).map(([key, value]) => {
+          if (key.includes('fullName') && value) {
+            return (
+              <h1 className="name" key={key}>
+                {value}
+              </h1>
+            );
+          } else if (key.includes('professionalTitle') && value) {
+            return (
+              <h2 className="title" key={key}>
+                {value}
+              </h2>
+            );
+          } else if (key.includes('professionalSummary') && value) {
+            return (
+              <pre className="summary" key={key}>
+                {value}
+              </pre>
+            );
+          }
+        })}
+      </div>
+      <div className="personal-data">
+        {Object.entries(cvData.personalDetails).map(([key, value]) => {
+          if (key.includes('email') && value) {
+            return (
+              <div className="entry email" key={key}>
+                <img src={emailIcon} alt="" />
+                <p>{value}</p>
+              </div>
+            );
+          } else if (key.includes('phone') && value) {
+            return (
+              <div className="entry phone" key={key}>
+                <img src={phoneIcon} alt="" />
+                <p>{value}</p>
+              </div>
+            );
+          } else if (key.includes('address') && value) {
+            return (
+              <div className="entry address" key={key}>
+                <img src={addressIcon} alt="" />
+                <p>{value}</p>
+              </div>
+            );
+          } else if (key.includes('links')) {
+            return (
+              <>
+                {value.map((value, index) => {
+                  if (value) {
+                    return (
+                      <div className="entry link" key={index}>
+                        <img src={linkIcon} alt="" />
+                        <p>{value}</p>
+                      </div>
+                    );
+                  }
+                })}
+              </>
+            );
+          }
+        })}
+      </div>
+      <div className="education">
+        <h3>Education</h3>
+      </div>
+      <div className="work-experience">
+        <h3>Work experience</h3>
+      </div>
+      <div className="skills">
+        <h3>Skills</h3>
       </div>
     </div>
   );
