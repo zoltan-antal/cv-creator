adcbafba8391c6e03441944239dbb51b0a34a46d
zolantal <zoltan_antal@outlook.com>
2023-10-04 17:19:48 +0100

    Add and style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,23 @@
+import '../styles/Footer.css';
+import githubIcon from '../assets/icons/github.svg';
+
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <pre>Built by </pre>
+      <a className="user" href="https://github.com/zoltan-antal">
+        <pre>Zoltan Antal</pre>
+      </a>
+      <pre> </pre>
+      <a href="https://github.com/zoltan-antal">
+        <img src={githubIcon} alt="GitHub" />
+      </a>
+      <pre> | </pre>
+      <a className="repo" href="https://github.com/zoltan-antal/cv-generator">
+        Source code
+      </a>
+    </footer>
+  );
 }
 
 export default Footer;
