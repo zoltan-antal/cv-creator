4603340bbf8b175a603e01dbebe5f0d1c875c69a
zolantal <zoltan_antal@outlook.com>
2024-02-09 16:08:40 +0000

    [backend] Change route of ping

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ require('express-async-errors');
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/ping', (request, response) => {
+app.get('/ping', (request, response) => {
   response.send('Hello');
 });
 
