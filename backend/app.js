a82d46081255211b5df591e0dbbb4fed6dc1205a
zolantal <zoltan_antal@outlook.com>
2024-02-09 12:54:11 +0000

    Set up basic backend with Express

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('express-async-errors');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/ping', (request, response) => {
+  response.send('Hello');
+});
+
+module.exports = app;
---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+const config = require('./utils/config');
+// const { connectToDatabase } = require('./utils/db');
+
+const start = async () => {
+  // await connectToDatabase();
+  app.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`);
+  });
+};
+
+start();
