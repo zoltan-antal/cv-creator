b6c2e2b54a039c83d88581cae3e2fe944c88d523
zolantal <zoltan_antal@outlook.com>
2024-02-09 17:11:28 +0000

    [backend] Add morgan for logging requests

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('express-async-errors');
+const morgan = require('morgan');
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.get('/ping', (request, response) => {
   response.send('Hello');
