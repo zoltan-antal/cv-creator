c8d1df846c45dce70b6f15ee201b8049f35436c9
zolantal <zoltan_antal@outlook.com>
2024-02-16 14:37:04 +0000

    [frontend] Uncouple user action creators from cv data initialising action creator; preserve current CV view after login/signup

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import loginService from '../services/login';
 import userService from '../services/user';
 import cvService from '../services/cv';
-import { initialiseCVData, restoreInitialCVData } from './cvDataSlice';
+import { restoreInitialCVData, setCVData } from './cvDataSlice';
 import blankCv from '../dataStructures/blankCv';
 
 const initialState = null;
@@ -24,7 +24,7 @@ const userSlice = createSlice({
 // Action creators
 
 const retrieveLoggedUser = () => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
     const token = localStorage.getItem('cvCreatorAuthToken');
     if (!token) {
       return;
@@ -35,7 +35,12 @@ const retrieveLoggedUser = () => {
     dispatch(setUser(userFull));
     const fetchedCVList = await cvService.getCVs();
     localStorage.setItem('cvList', JSON.stringify(fetchedCVList));
-    dispatch(initialiseCVData());
+    let cvId = getState().cvData.selectedCvId;
+    if (!fetchedCVList.find((cv) => cv.id === cvId)) {
+      cvId = fetchedCVList[0].id;
+      localStorage.setItem('cvId', cvId);
+    }
+    dispatch(setCVData({ cvList: fetchedCVList, cvId }));
   };
 };
 
@@ -49,19 +54,27 @@ const loginUser = ({ username, password }) => {
     const cvListToKeep = cvList.filter(
       (cv) => !_.isEqual(_.omit(cv, ['id']), _.omit(blankCv, ['id']))
     );
-    cvListToKeep.forEach(
-      async (cv) => await cvService.createCV(_.omit(cv, ['id']))
-    );
-    let userFull = await userService.getUser();
+    let cvId = getState().cvData.selectedCvId;
+    for (const cv of cvListToKeep) {
+      const createdCV = await cvService.createCV(_.omit(cv, ['id']));
+      if (cv.id === cvId) {
+        cvId = createdCV.id;
+      }
+    }
     let fetchedCVList = await cvService.getCVs();
+    let userFull = await userService.getUser();
     if (cvListToKeep.length === 0 && fetchedCVList.length === 0) {
       await cvService.createCV(_.omit(blankCv, ['id']));
       fetchedCVList = await cvService.getCVs();
       userFull = await userService.getUser();
     }
+    if (cvListToKeep.length === 0) {
+      cvId = fetchedCVList[0].id;
+    }
     dispatch(setUser(userFull));
+    dispatch(setCVData({ cvList: fetchedCVList, cvId }));
     localStorage.setItem('cvList', JSON.stringify(fetchedCVList));
-    dispatch(initialiseCVData());
+    localStorage.setItem('cvId', cvId);
   };
 };
 
