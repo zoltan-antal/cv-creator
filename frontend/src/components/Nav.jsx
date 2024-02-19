7eb3eb200f7be372e2692179575197d5faf22a04
zolantal <zoltan_antal@outlook.com>
2024-02-19 16:11:42 +0000

    [frontend] Implement PDF saving

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,5 +1,7 @@
 import '../styles/Nav.css';
 import { useRef } from 'react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 import Button from './Button';
 import ConfirmDialog from './ConfirmDialog';
 import CVListDialog from './CVListDialog';
@@ -16,8 +18,38 @@ const Nav = () => {
   const selectCVDialogRef = useRef(null);
   const loginDialogRef = useRef(null);
 
+  const generatePdf = async () => {
+    const input = document.querySelector('.page');
+    const canvas = await html2canvas(input, { scale: 4 });
+
+    const ctx = canvas.getContext('2d');
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    // Invert colours
+    for (let i = 0; i < data.length; i += 4) {
+      data[i] = 255 - data[i];
+      data[i + 1] = 255 - data[i + 1];
+      data[i + 2] = 255 - data[i + 2];
+    }
+    // Apply 100% greyscale
+    for (let i = 0; i < data.length; i += 4) {
+      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+      const newColour = avg <= 127 ? 0 : 255;
+      data[i] = newColour;
+      data[i + 1] = newColour;
+      data[i + 2] = newColour;
+    }
+    ctx.putImageData(imageData, 0, 0);
+
+    const pdf = new jsPDF('p', 'mm', 'a4');
+    pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 210, 297);
+    const blobURL = pdf.output('bloburl');
+    window.open(blobURL, '_blank');
+  };
+
   return (
     <nav>
+      <Button name={'Print/Save'} className="dark" onClick={generatePdf} />
       <Button
         name={'Clear'}
         className="dark red"
