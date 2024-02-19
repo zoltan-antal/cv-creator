b666f90391fda99c3dd5c41e8c9956d3443a43ae
zolantal <zoltan_antal@outlook.com>
2024-02-19 16:58:03 +0000

    [frontend] Rename Main component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import './styles/App.css';
 import Header from './components/Header';
-import Main from './components/MainComponent';
+import Main from './components/Main';
 import Footer from './components/Footer';
 import { useDispatch } from 'react-redux';
 import { initialiseCVData } from './slices/cvDataSlice';
