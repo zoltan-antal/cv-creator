0a026cebf40da82977fd4e8019ecb0a5365d0a53
zolantal <zoltan_antal@outlook.com>
2024-04-25 11:31:13 +0100

    [frontend] Make links open in new tab

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -5,15 +5,29 @@ const Footer = () => {
   return (
     <footer>
       <pre>Built by </pre>
-      <a className="user" href="https://zoltan-antal.dev">
+      <a
+        className="user"
+        href="https://zoltan-antal.dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <pre>Zoltan Antal</pre>
       </a>
       <pre> </pre>
-      <a href="https://github.com/zoltan-antal">
+      <a
+        href="https://github.com/zoltan-antal"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={githubIcon} alt="GitHub" />
       </a>
       <pre> | </pre>
-      <a className="repo" href="https://github.com/zoltan-antal/cv-creator">
+      <a
+        className="repo"
+        href="https://github.com/zoltan-antal/cv-creator"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Source code
       </a>
     </footer>
